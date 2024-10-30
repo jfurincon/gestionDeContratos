ebb733a56923e60b9383dd0b5b8b45eba9a3fa6a
Paula Gonzalez <147997070+paula-gonzalez-23@users.noreply.github.com>
2024-10-30 08:26:45 -0500

    Add files via upload

--- INDIVIDUAL/Paula/index.js
+++ INDIVIDUAL/Paula/index.js
@@ -19,3 +19,65 @@ mongoose.connect('mongodb://localhost:27017/BDMongo',{
 app.listen(PORT,()=>{
     console.log('Servidor ejecutandose sobre el puerto: ${PORT}');
 });
+
+//Agregar rutas para manipular user 
+
+const User = require ('./user.js') //ruta del archivo user 
+
+//Registrar un usuario nuevo 
+
+app.post('/users', async(req,res)=>{
+    try{
+        const user = new User (req.body);
+        await user.save();
+        res.status(201).send(user);
+
+    }catch (error){
+
+        res.status(400).send(error)
+    }
+})
+
+//Consultar los usuarios
+
+app.get('/users', async(req,res)=>{
+    try{
+        const users = await User.find();
+        res.status(201).send(users);
+
+    }catch (error){
+
+        res.status(500).send(error)
+    }
+})
+
+//Consultar usuario por id 
+
+app.get('/users/:id', async(req,res)=>{
+    try{
+        const users = await User.findById(req.params.id);
+        if(!user)return response.status(404).send();
+        res.status(201).send(users);
+
+    }catch (error){
+
+        res.status(500).send(error)
+    }
+})
+
+//Actualizar un usuario 
+
+app.get('/users/:id', async(req,res)=>{
+    try{
+        const users = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+        if(!user)return response.status(404).send();
+        res.status(201).send(users);
+
+    }catch (error){
+
+        res.status(400).send(error)
+    }
+})
+
+//Eliminar
+
